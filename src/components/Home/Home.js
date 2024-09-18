f126a7dc31230f7c42aaff973be964255fa584cc
sangvishtechnologies <info@sangvish.com>
2024-09-18 10:58:18 +0530

    Your commit message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> mohammed Siraj </strong>
+                <strong className="main-name"> mohammed  sdss Siraj </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
